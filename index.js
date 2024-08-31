fb7ee2f85a747db5dde7676af317c494e84d6bb7
saloni <salonijaiswal.7412@gmail.com>
2024-08-31 13:32:04 +0530

    separation of concerns

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const app=express();
 
 const cookieParser=require("cookie-parser");
 const path=require("path");
-const exp = require('constants');
+
+const db=require("./config/mongoose-connection");
+const ownersRouter=require("./routes/ownersRouter");
+const usersRouter=require("./routes/usersRouter");
+const productsRouter=require("./routes/productsRouter");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
+
+app.use("/owners",ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productsRouter);
 
 app.get("/",(req,res)=>{
     res.send("Hello");
